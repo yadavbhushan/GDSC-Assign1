7b4d29f2d572b2988bb92703fadc59fdfc6dc580
Bhushan kumar <bhushanbasantpur@gmai.com>
2023-09-19 15:52:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './style.css';
+import data from "./data.json";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="templateContainer">
+        <div className="searchInput_Container">
+          <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }} />
+        </div>
+        <div className="template_Container">
+          {
+            data 
+              .filter((val) => {
+                if(searchTerm == ""){
+                  return val;
+                }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                  return val;
+                }
+              })
+              .map((val) => {
+                return(
+                  <div className="template" key={val.id}>
+                      <img src={val.images} alt="" />
+                     
+                      <h3>{val.title}</h3>
+                      <h3>{val.brand}</h3>
+                      <h3>{val.category}</h3>
+                      <h3>{val.description}</h3>
+                      <p className="price">${val.price}</p>
+                     
+                  </div> 
+                )
+              })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
